34208c5fdec8a23f827b85e1032b22f370901889
Guillaume Lozenguez <guillaume@drods.net>
2021-01-05 16:56:55 +0100

    Fucking services

--- src/prof/scripts/tictac.py
+++ src/prof/scripts/tictac.py
@@ -1,12 +1,22 @@
-#!/bin/python3
+#!/usr/bin/env python3
 import rospy
 from std_msgs.msg import String
+from prof import srv
 
 # Program parameter:
 rospy.init_node('tictac', anonymous=True)
 pub = rospy.Publisher('tic', String, queue_size=10)
-rate = rospy.Rate(1) # 10hz
+rate = rospy.Rate(1) # 1hz
 msgs= ['tic', 'tac']
+
+# ROS callback
+def rate_service( data ):
+    global rate
+    rate= rospy.Rate( data.Value )
+    return 1
+
+# ROS node configuration
+rospy.Service('rate', srv.Rate, rate_service)
 
 # Program loop:
 i= 0
